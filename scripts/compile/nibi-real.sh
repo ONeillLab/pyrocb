2299006f1d559654df1bf3061ca3ee6faba4a5ba
Su386 <83100266+Su386yt@users.noreply.github.com>
2026-07-17 16:47:01 -0400

    Moved downloading portion of compile script to setup

--- scripts/compile/nibi-real.sh
+++ scripts/compile/nibi-real.sh
@@ -21,13 +21,8 @@ else
 fi
 
 
-cd $PCB_OUT_DIR
-git clone https://github.com/wrf-model/WPS.git
-
 cd $PCB_OUT_DIR/WPS
 export WRF_DIR=$PCB_OUT_DIR/WRF-SFIRE    # <-- points at WRF-SFIRE, not WRF-Fire
-./configure
-echo "WPS Configured!"
 
 ./compile 2>&1 | tee compile_wps.log
 echo "WPS Compiled!"
--- scripts/setup/wrf-sfire.sh
+++ scripts/setup/wrf-sfire.sh
@@ -5,3 +5,11 @@ cd WRF-SFIRE
 
 bash $PCB_SCRIPTS_DIR/override/configure-wrf.sh
 echo [LOG] Overrode configure file with fixed configure files.
+
+cd $PCB_OUT_DIR
+git clone https://github.com/wrf-model/WPS.git
+
+cd $PCB_OUT_DIR/WPS
+export WRF_DIR=$PCB_OUT_DIR/WRF-SFIRE    # <-- points at WRF-SFIRE, not WRF-Fire
+./configure
+echo "WPS Configured!"
