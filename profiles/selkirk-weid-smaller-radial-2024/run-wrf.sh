02be79a2fd9e51bfc8c44ad9c68557314638571e
Su386 <83100266+Su386yt@users.noreply.github.com>
2026-08-02 10:31:40 -0400

    Smaller selkirk domains

---
+++ profiles/selkirk-weid-smaller-radial-2024/run-wrf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --account=def-oneill
+#SBATCH --job-name=wrf_selkirk_wrf_t1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=96
+#SBATCH --mem=0
+#SBATCH --output=/scratch/su386/pyrocb5/out/logs/selkirk-weid-radial-2024/%x_%j.out
+#SBATCH --error=/scratch/su386/pyrocb5/out/logs/selkirk-weid-radial-2024/%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=s.dosreis@mail.utoronto.ca
+
+source $SCRATCH/pyrocb/alliance_can_shell_setup.sh 
+module load StdEnv/2023 gcc/12 openmpi/4.1.5
+
+PROFILE=`cat $PCB_OUT_DIR/profile/profile`
+echo $PROFILE
+
+# Remove old history files. Make sure you saved files before!
+cd $PCB_OUTPUT_DIR/$PROFILE/history
+rm -rf wrfout_d0*
+
+cd $PCB_OUTPUT_DIR/$PROFILE/wrfout
+rm -f rsl.error.* rsl.out.*
+
+cd $PCB_OUTPUT_DIR/$PROFILE/real_em
+rm -f rsl.error.* rsl.out.*
+ulimit -s unlimited
+echo "Starting wrf.exe at $(date)"
+srun bash -c "ulimit -s unlimited && ./wrf.exe" 2>&1 | tee $PCB_LOGS_DIR/$PROFILE/wrf.log
+echo "wrf.exe exit code: $?"
+echo "Job finished at $(date)"
+
+tail -n 50 rsl.out.0000
