a28dd2c13edfef76963c7dfbe705816f959d9e33
Su386 <83100266+Su386yt@users.noreply.github.com>
2026-08-03 23:48:46 -0400

    Trying to get efficiency gains

--- profiles/selkirk-weid-wind-reduction-2024/run-wrf.sh
+++ profiles/selkirk-weid-wind-reduction-2024/run-wrf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-oneill
 #SBATCH --job-name=wrf_selkirk_wrf_t1
-#SBATCH --time=18:00:00
-#SBATCH --nodes=4
+#SBATCH --time=24:00:00
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=192
 #SBATCH --mem=0
 #SBATCH --output=/scratch/su386/pyrocb/out/logs/selkirk-weid-2024/%x_%j.out
