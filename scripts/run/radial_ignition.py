99378a488ddbfd7bda118c42380f8789663b5dd8
Su386 <83100266+Su386yt@users.noreply.github.com>
2026-08-12 16:12:14 -0400

    oops

--- scripts/run/radial_ignition.py
+++ scripts/run/radial_ignition.py
@@ -21,7 +21,7 @@ with open(f'{sys.argv[1]}/profile/config.json', 'r') as f:
     run_start_time_str = data["run_start_time"]
     ignition_start_time_str = data["ignition_start_time"]
     bounds = tuple(data["bounds"])
-with open(f"{directory}/profile/profile", 'r') as p:
+with open(f"{sys.argv[1]}/profile/profile", 'r') as p:
     name = p.read().strip()
 print(name)
 
